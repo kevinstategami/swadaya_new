bb32fda9ed92106d908311b9dfc01b12f11f109b
dwikay <aliansyahkay@gmail.com>
2022-02-19 15:41:23 +0700

    fixing title

--- resources/views/registrasi/home/wallet/tarik.blade.php
+++ resources/views/registrasi/home/wallet/tarik.blade.php
@@ -5,8 +5,8 @@
       <meta name="apple-mobile-web-app-capable" content="yes">
       <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
       <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover" />
-      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
-      <title>Swadaya Utama</title>
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+      <title>Songgo Mas</title>
       <link rel="stylesheet" type="text/css" href="{{asset('registrasi/styles/bootstrap.css')}}" />
       <link rel="stylesheet" type="text/css" href="{{asset('registrasi/fonts/bootstrap-icons.css')}}" />
       <link rel="stylesheet" type="text/css" href="{{asset('styles/style.css')}}" />
